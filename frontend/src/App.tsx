928c1ccbec925f555bb6f4d8b05294bb131eafa1
Alexander Borisenko <wealthybora@gmail.com>
2018-04-07 19:26:14 +0300

    fx tslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import "./App.css";
+import * as React from 'react';
+import './App.css';
 
-const logo = require("./logo.svg");
+const logo = require('./logo.svg');
 
 export interface Props {
   name: string;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(
   <App name="Moshe" city="United States" />,
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
