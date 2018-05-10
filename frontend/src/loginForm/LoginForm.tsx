bf9b496496305571f34707a51d8ef8995575c13f
Erez Hochman <erez@leadcoin.network>
2018-05-10 12:29:06 +0300

    midway commit on the signup/login, because of power shortage

--- frontend/src/loginForm/LoginForm.tsx
+++ frontend/src/loginForm/LoginForm.tsx
@@ -1,12 +1,67 @@
-import * as React from 'react';
-import Button from 'material-ui/Button';
+import * as React from 'react'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
+
+interface stateProps {
+  email: string
+  password: string
+}
 
 export class LoginForm extends React.Component {
+  state: stateProps = {
+    email: '',
+    password: ''
+  }
+
+  pwordChange(event: any) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  emailChange(event: any) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
   render() {
     return (
-      <Button variant="raised" color="primary">
-        Hi Bora
-      </Button>
+      <div className="loginForm">
+        <div className="localLogin">
+          <div>
+            <TextField
+              id="email"
+              label="Email"
+              className="emailField"
+              value={this.state.email}
+              onChange={this.emailChange.bind(this)}
+              margin="normal" />
+          </div>
+          <div>
+            <TextField
+              id="password"
+              label="Password"
+              className="pwordField"
+              value={this.state.password}
+              onChange={this.pwordChange.bind(this)}
+              type="password"
+              margin="normal" />
+          </div>
+          <Button variant="raised" color="primary">
+            Login
+          </Button>
+        </div>
+        <div> Or, you can sign in using </div>
+        <div className="external_login_container">
+          <div> <Button variant="raised" color="primary"> Facebook </Button> </div>
+          <div> <Button variant="raised" color="primary"> Google </Button> </div>
+        </div>
+        <div className="signup_cta">
+          <span> Not a user? </span>
+          <span> Sign-up now! </span>
+          </div>
+      </div>
     );
   }
 }
