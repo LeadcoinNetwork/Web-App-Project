6415c9f3ab049f296c1511a66503db2722e10c0d
Meir Cohen <meir@leadcoin.network>
2018-05-27 17:55:26 +0300

    WIP - table GUI

--- frontend/src/Table/index.tsx
+++ frontend/src/Table/index.tsx
@@ -1,7 +1,28 @@
+import './style.css';
+
 import * as React from 'react';
+import Checkbox from '../leadcoin_ui/Checkbox';
 
 const fields = require('./fields.json');
 const records = require('./records.json');
+
+const dinamicColsCount = (fields:any) => (
+  fields.filter((f:any) => (
+    f.maxWidth === 'auto')
+  ).length
+);
+
+const widthOfStaticCols = (fields:any) => {
+  let width = 0;
+
+  fields.forEach((f:any) => {
+    if (f.maxWidth !== 'auto') {
+      width += Number(f.maxWidth.substr(0, f.maxWidth.length - 2));
+    }
+  })
+
+  return width;
+};
 
 export interface IField {
   name:string;
@@ -30,9 +51,9 @@ class Table extends React.Component <ITable> {
   }
   render() {
     return (
-      <section className='ldc-table'>
-        <THead fields={fields} />
-        <div className='t-body'></div>
+      <section className='ldc-table' >
+        <THead fields={fields} colCount={dinamicColsCount(fields)} staticColsWidth={widthOfStaticCols(fields)} />
+        <TBody fields={fields} records={records} colCount={dinamicColsCount(fields)} staticColsWidth={widthOfStaticCols(fields)} />
         <div className='t-footer'></div>
       </section>
 
@@ -40,22 +61,72 @@ class Table extends React.Component <ITable> {
   }
 }
 
-const THead = ({
-  fields
-}:{fields:IField[]}) => (
+const THead = (props:any) => (
   <div className='t-head'>
+    <THRow {...props} />
+  </div>
+);
+
+const THRow = (props:any) => (
+  <div className='th-row'>
     {
-      fields.map((f:any) => (
-        <HeadCol key={f.name} name={f.name} />
+      props.fields.map((f:any) => (
+        <THRCol key={f.name} colCount={props.colCount} staticColsWidth={props.staticColsWidth} field={f} />
+      ))
+    }
+  </div>
+)
+
+const THRCol = ({
+  field,
+  colCount,
+  staticColsWidth,
+}:any) => (
+  <div key={field.name} className='thr-col' style={{
+    width: `calc((100% - ${staticColsWidth}px) / ${colCount})`,
+    maxWidth: field.maxWidth,
+    minWidth: field.minWidth,
+  }}>
+    {field.name}
+  </div>
+);
+
+const TBody = (props:any) => (
+  <div className='t-body'>
+    {
+      props.records.map((r:any) => (
+        <TBRow key={r.id} {...r} {...props} />
       ))
     }
   </div>
 );
 
-const HeadCol = ({
-  name
-}:any) => (
-  <div>{name}</div>
+const TBRow = (props:any) => (
+  <div className='tb-row'>
+    <div className='tbr-checkbox'>
+      <Checkbox checked={false} onClick={() => console.log(props.id)} />
+    </div>
+    {
+      props.fields.map((f:any) => (
+        <TRCol key={f.key} colCount={props.colCount} staticColsWidth={props.staticColsWidth} field={f} value={props[f.key]} />
+      ))
+    }
+  </div>
 );
+
+const TRCol = ({
+  field,
+  value,
+  colCount,
+  staticColsWidth,
+}:any) => (
+  <div className='tbr-col' style={{
+    width: `calc((100% - ${staticColsWidth}px) / ${colCount})`,
+    maxWidth: field.maxWidth,
+    minWidth: field.minWidth,
+  }}>
+    {value}
+  </div>
+)
 
 export default Table;
