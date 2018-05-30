92c9ece4ae0c18791d8a29955f3438e2c770cd9f
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-05-30 13:41:06 +0300

    create first stories decorations (createStoryInDEsignDecoration)

--- frontend/stories/index.js
+++ frontend/stories/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Button from '../src/components/Button';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Button from "../src/components/Button";
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <MuiThemeProvider>
-      <Button label="Hello"/>
-    </MuiThemeProvider>
-  ))
+function createStoryInDesignDecoration(nameOfStory) {
+  return storiesOf(nameOfStory, module).addDecorator(getStories => (
+    <MuiThemeProvider children={getStories()} />
+  ));
+}
+
+createStoryInDesignDecoration("a/a")
+  .add("with text/a", () => <Button label="Hello1" />)
+  .add("with text/b", () => <Button label="Hello2" />);
+
+createStoryInDesignDecoration("a/b")
+  .add("with text/a", () => <Button label="Hello1" />)
+  .add("with text/b", () => <Button label="Hello2" />);
