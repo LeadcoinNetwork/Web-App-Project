fd6cbe94d06ac7bd4137b59f45eefe6ca0da58f3
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-05-31 18:05:38 +0300

    backend: remove old typescript files

--- backend/src/index.js
+++
@@ -1,6 +0,0 @@
-// import * as express from 'express';
-var app = express();
-
-app.get("/health", function(req, res) {
-  res.send("server is online");
-});
--- backend/src/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-var app = express();
-
-app.get("/health", (req, res) => {
-  res.send("server is online");
-});
