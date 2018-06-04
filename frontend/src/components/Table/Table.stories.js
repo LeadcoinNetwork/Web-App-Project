c6eaa5ec007393d9751c43364b6980df1dee4ffe
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-06-04 12:42:18 +0300

    FRONTEND: create empty stories for table

--- frontend/src/components/Table/Table.stories.js
+++ frontend/src/components/Table/Table.stories.js
@@ -16,7 +16,7 @@ createStoryInDesignDecoration("Table").add("with leads", () => (
   <Table
     fields={fields}
     records={leads}
-    multipleSelectionButton="Buy Selected Leads"
+    multipleSelectionButton="Buy ${number} Leads"
     multipleSelectionAction={console.log}
     recordMainButton="Buy"
     recordMainAction={console.log}
@@ -28,6 +28,49 @@ createStoryInDesignDecoration("Table").add("without leads", () => (
     fields={fields}
     showOnZeroRecords={<div>No Leads</div>}
     records={[]}
+    multipleSelectionButton="Buy Selected Leads"
+    multipleSelectionAction={console.log}
+    recordMainButton="Buy"
+    recordMainAction={console.log}
+    onScrollBottom={cb => cb()}
+  />
+));
+
+createStoryInDesignDecoration("Table").add("with onSort action", () => (
+  <Table
+    fields={fields}
+    showOnZeroRecords={<div>No Leads</div>}
+    onSort={key => {
+      alert("sorted by field.key: " + key);
+    }}
+    records={leads}
+    multipleSelectionButton="Buy Selected Leads"
+    multipleSelectionAction={console.log}
+    recordMainButton="Buy"
+    recordMainAction={console.log}
+    onScrollBottom={cb => cb()}
+  />
+));
+
+createStoryInDesignDecoration("Table").add("with sort asc", () => (
+  <Table
+    fields={fields}
+    showOnZeroRecords={<div>No Leads</div>}
+    sortedBy={{ key: "state", direction: "asc" }}
+    records={leads}
+    multipleSelectionButton="Buy Selected Leads"
+    multipleSelectionAction={console.log}
+    recordMainButton="Buy"
+    recordMainAction={console.log}
+    onScrollBottom={cb => cb()}
+  />
+));
+createStoryInDesignDecoration("Table").add("with sort desc", () => (
+  <Table
+    fields={fields}
+    showOnZeroRecords={<div>No Leads</div>}
+    sortedBy={{ key: "state", direction: "desc" }}
+    records={leads}
     multipleSelectionButton="Buy Selected Leads"
     multipleSelectionAction={console.log}
     recordMainButton="Buy"
