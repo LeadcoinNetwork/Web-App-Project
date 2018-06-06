0aa8e09d51ac6ab74a89ba4abd205b27f57f8410
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-06-06 15:58:52 +0300

    cover header to function

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,11 +3,8 @@ import Snacbar from "material-ui/Snackbar";
 import Button from "Components/Button";
 import { Link } from "react-router-dom";
 
-class Header extends React.Component {
-  constructor(props, context) {
-    super(props);
-  }
-  getSnackbarStyle(type) {
+function Header(props) {
+  function getSnackbarStyle(type) {
     let color, backgroundColor;
 
     switch (type) {
@@ -30,50 +27,49 @@ class Header extends React.Component {
 
     return { color, backgroundColor };
   }
-  render() {
-    let props = this.props,
-      currentNotification = props.notifications.current;
-    currentNotification.style = this.getSnackbarStyle(currentNotification.type);
-    var routes = [
-      "/admin/login",
-      "/admin/leads",
-      "/admin/transactions",
-      "/users/signup",
-      "/users/complete-registration",
-      "/users/email-confirmation",
-      "/users/login",
-      "/users/settings",
-      "/users/notifications",
-      "/users/payments",
-      "/users/withdrawal",
-      "/leads/buy",
-      "/leads/sell",
-      "/leads/my",
-      "/leads/new",
-      "/leads/csv-upload",
-      "/leads/csv-mapping",
-      "/leads/checkout",
-      "/leads/1/dispute"
-    ];
-    return (
-      <div className="ldc-header">
-        {routes.map(path => (
-          <span>
-            <Link to={path}>{path}</Link>&nbsp;&nbsp;
-          </span>
-        ))}
-        <Snacbar
-          open={!!currentNotification.message}
-          message={currentNotification.message || ""}
-          autoHideDuration={50000}
-          bodyStyle={currentNotification.style}
-          contentStyle={currentNotification.style}
-        />
-        <br />
-        <br />
-      </div>
-    );
-  }
+
+  var currentNotification = props.notifications.current;
+  currentNotification.style = getSnackbarStyle(currentNotification.type);
+  var routes = [
+    "/admin/login",
+    "/admin/leads",
+    "/admin/transactions",
+    "/users/signup",
+    "/users/complete-registration",
+    "/users/email-confirmation",
+    "/users/login",
+    "/users/settings",
+    "/users/notifications",
+    "/users/payments",
+    "/users/withdrawal",
+    "/leads/buy",
+    "/leads/sell",
+    "/leads/my",
+    "/leads/new",
+    "/leads/csv-upload",
+    "/leads/csv-mapping",
+    "/leads/checkout",
+    "/leads/1/dispute"
+  ];
+
+  return (
+    <div className="ldc-header">
+      {routes.map(path => (
+        <span>
+          <Link to={path}>{path}</Link>&nbsp;&nbsp;
+        </span>
+      ))}
+      <Snacbar
+        open={!!currentNotification.message}
+        message={currentNotification.message || ""}
+        autoHideDuration={50000}
+        bodyStyle={currentNotification.style}
+        contentStyle={currentNotification.style}
+      />
+      <br />
+      <br />
+    </div>
+  );
 }
 
 export default Header;
