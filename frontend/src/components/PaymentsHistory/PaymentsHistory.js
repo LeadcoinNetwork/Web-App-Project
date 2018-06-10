6d885d18cd5fbe6066ae954d3ea3e014e8a05076
Meir Cohen <meir@leadcoin.network>
2018-06-10 18:51:26 +0300

    add balance and escrow balance to payments

--- frontend/src/components/PaymentsHistory/PaymentsHistory.js
+++ frontend/src/components/PaymentsHistory/PaymentsHistory.js
@@ -2,9 +2,19 @@ import React, { Component } from "react"
 import { Time } from "../../utils/time"
 
 const PaymentsHistory = props =>
-  props.payments.map(payment => (
-    <div key={payment.id}>
-      <b>{Time.format(payment.timestamp)}</b> {payment.message}
+  props.payments.map(p => (
+    <div key={p.id}>
+      <time>{new Date(p.timestamp).toLocaleString()}</time>
+      <div>{p.message}</div>
+      <div>
+        <b>Escrow balance:</b>
+        {p.escrowBalance}
+      </div>
+      <div>
+        <b>Balance:</b>
+        {p.balance}
+      </div>
+      <br />
     </div>
   ))
 
