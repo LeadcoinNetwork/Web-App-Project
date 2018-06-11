a29061fbdc9ed333903f4b336e44988bd7e7174b
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-06-11 13:00:26 +0300

    fix redux middleware

--- frontend/src/storybook-utils/withRedux.js
+++ frontend/src/storybook-utils/withRedux.js
@@ -1,4 +1,7 @@
 import { action, configureActions } from "@storybook/addon-actions"
 export function storyReduxLogger({ getState }) {
-  return next => action("Redux Action")
+  return next => _action => {
+    action("Redux Action")
+    next(_action)
+  }
 }
