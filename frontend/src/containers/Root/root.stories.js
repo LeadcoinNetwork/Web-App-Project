101fffa6e214ea5d666a90cb33a83fd049c8adac
Meir Cohen <meir@leadcoin.network>
2018-06-12 10:51:03 +0300

    add all containers to storybook

--- frontend/src/containers/Root/root.stories.js
+++ frontend/src/containers/Root/root.stories.js
@@ -15,6 +15,7 @@ import { storyReduxLogger } from "../../storybook-utils/withRedux"
 const paymentsMock = require("../../mocks/payments.json")
 
 function getPaymentsSaga(mockType) {
+  console.log(mockType)
   return function*() {
     while (true) {
       yield take("GET_PAYMENTS")
@@ -54,10 +55,181 @@ storiesOf("App")
       </Provider>
     )
   })
-  .add("payments Page", () => {
+  .add("Payments", () => {
     return (
       <Provider store={store}>
         <MemoryRouter initialEntries={["/payments"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - buy", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/buy"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - sell", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/sell"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - my", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/my"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - new", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/new"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - csv upload", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/csv-upload"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - csv mapping", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/csv-mapping"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - checkout", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/checkout"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Leads - dispute - lead 1", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/leads/1/dispute"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - signup", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/users/signup"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - complete registration", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={["/users/complete-registration"]}
+          initialIndex={0}
+        >
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - email confirmation", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={["/users/email-confirmation"]}
+          initialIndex={0}
+        >
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - login", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/users/login"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - settings", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/users/settings"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - notification", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={["/users/notifications"]}
+          initialIndex={0}
+        >
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Users - withdrawal", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/users/withdrawal"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Admin - login", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/admin/login"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Admin - leads", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/admin/leads"]} initialIndex={0}>
+          <Root />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  .add("Admin - transactions", () => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/admin/transactions"]} initialIndex={0}>
           <Root />
         </MemoryRouter>
       </Provider>
