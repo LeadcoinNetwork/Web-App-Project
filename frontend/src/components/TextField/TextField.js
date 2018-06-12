843be510d1aacd97cdba402edac793399b25c9a7
Noam Silver <noam@leadcoin.network>
2018-06-12 11:12:39 +0300

    reverted previous changes

--- frontend/src/components/TextField/TextField.js
+++ frontend/src/components/TextField/TextField.js
@@ -2,7 +2,6 @@ import React from "react"
 import MaterialTextField from "material-ui/TextField"
 
 const TextField = ({
-  name,
   label,
   hintText,
   value,
@@ -11,7 +10,6 @@ const TextField = ({
   onChange,
 }) => (
   <MaterialTextField
-    name={name}
     value={value}
     type={type}
     floatingLabelText={label}
