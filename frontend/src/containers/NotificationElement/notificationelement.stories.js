0cf094ed469878fdcb17798682bbea3f2bc2afa2
Noam Silver <noam@leadcoin.network>
2018-06-14 13:09:11 +0300

    finished the design and actions of the NotificationElement and added NotificationElement stories

--- frontend/src/containers/NotificationElement/notificationelement.stories.js
+++ frontend/src/containers/NotificationElement/notificationelement.stories.js
@@ -14,18 +14,25 @@ storiesOf("App/Notification Element", module)
 
   .add("empty opened", () => {
     var { store, story } = createStoreAndStory({ component: NotificationElement })
+    store.dispatch(actions.notifications.showNotifications([], 0))
     return story
   })
 
   .add("have 6 unread 4", () => {
     var { store, story } = createStoreAndStory({ component: NotificationElement })
-    store.dispatch(actions.notifications.updateNotifications(notifications, notifications.filter(notification => notification.unread).length))
+    store.dispatch(actions.notifications.updateNotifications(notifications.noti6un4, notifications.noti6un4.filter(notification => notification.unread).length))
     return story
   })
-
+  
   .add("have 6 unread 4 opened", () => {
     var { store, story } = createStoreAndStory({ component: NotificationElement })
-    store.dispatch(actions.notifications.updateNotifications(notifications, notifications.filter(notification => notification.unread).length))
+    store.dispatch(actions.notifications.updateNotifications(notifications.noti6un4, notifications.noti6un4.filter(notification => notification.unread).length))
     store.dispatch(actions.notifications.showNotifications())
+    return story
+  })
+  
+  .add("have 15 unread 14", () => {
+    var { store, story } = createStoreAndStory({ component: NotificationElement })
+    store.dispatch(actions.notifications.updateNotifications(notifications.noti15un14, notifications.noti15un14.filter(notification => notification.unread).length))
     return story
   })
