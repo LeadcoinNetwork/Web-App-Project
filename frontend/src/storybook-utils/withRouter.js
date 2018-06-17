96b166b987a0b748afe54e2bcc6ea3e676cd235b
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-06-17 16:44:19 +0300

    make history to work in storybook

--- frontend/src/storybook-utils/withRouter.js
+++ frontend/src/storybook-utils/withRouter.js
@@ -8,6 +8,10 @@ import rootReducer from "Reducers"
 import Root from "containers/Root"
 import createSagaMiddleware from "redux-saga"
 import RootSaga from "sagas"
+import { routerMiddleware, ConnectedRouter } from "react-router-redux"
+import { createBrowserHistory, createMemoryHistory } from "history"
+
+// const history = createBrowserHistory()
 
 export function createStoreAndStory({
   path,
@@ -15,10 +19,17 @@ export function createStoreAndStory({
   sagaFunction,
   connectToProductionSaga,
 }) {
-  var middlewares = [storyReduxLogger]
+  var history = createMemoryHistory({
+    initialEntries: [path],
+    initialIndex: 0,
+  })
+  const ROUTER_MIDDLEWARE = routerMiddleware(history)
+
+  var middlewares = [storyReduxLogger, ROUTER_MIDDLEWARE]
   if (sagaFunction || connectToProductionSaga) {
     var sagaMiddleware = createSagaMiddleware()
-    middlewares.unshift(sagaMiddleware)
+    // middlewares.unshift(sagaMiddleware)
+    middlewares.push(sagaMiddleware)
   }
   /**
    * Redux Store
@@ -42,9 +53,9 @@ export function createStoreAndStory({
       <Provider store={store}>
         <div>
           {path && (
-            <MemoryRouter initialEntries={[path]} initialIndex={0}>
+            <ConnectedRouter history={history}>
               <Root />
-            </MemoryRouter>
+            </ConnectedRouter>
           )}
           {component && <UpperCaseComponent />}
         </div>
