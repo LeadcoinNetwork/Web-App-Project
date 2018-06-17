6717b2d09c1f2491c7283ab94e188945aaa728d2
Noam Silver <noam@leadcoin.network>
2018-06-17 16:49:50 +0300

    added payments history types, actions and stories

--- frontend/src/reducers/payments.js
+++ frontend/src/reducers/payments.js
@@ -5,24 +5,30 @@ const initialState = {
   loading: true,
   lastRefresh: null,
   list: [],
+  error: "",
 }
 
 const payment = (state = initialState, action) => {
   switch (action.type) {
-    case types.PAYMENTS_HISTORY_UPDATE:
-      return {
-        ...state,
-        ...action.payload,
-      }
-    case types.PAYMENTS_HISTORY_START:
+    case types.PAYMENTS_HISTORY_LOADING_START:
       return {
         ...state,
         loading: true,
       }
-    case types.PAYMENTS_HISTORY_FINISH:
+    case types.PAYMENTS_HISTORY_LOADING_END:
       return {
         ...state,
         loading: false,
+      }
+    case types.PAYMENTS_HISTORY_UPDATE:
+      return {
+        ...state,
+        list: action.payload,
+      }
+    case types.PAYMENTS_HISTORY_ERROR:
+      return {
+        ...state,
+        error: action.payload,
       }
     default:
       return state
