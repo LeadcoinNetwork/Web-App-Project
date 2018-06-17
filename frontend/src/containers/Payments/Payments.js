861789be51a58b535cd523729104d0f28e50287c
Noam Silver <noam@leadcoin.network>
2018-06-17 16:52:29 +0300

    added Payments table with stories using Table component

--- frontend/src/containers/Payments/Payments.js
+++ frontend/src/containers/Payments/Payments.js
@@ -2,34 +2,57 @@ import React from "react"
 import { connect } from "react-redux"
 import * as Actions from "../../actions"
 import { Time } from "../../utils/time"
+import Table from "../../components/Table"
 
-const PaymentsHistory = ({ onRefresh, payments, isDeleteable }) => (
+const paymentsConfig = require("./payments_table.config.json")
+
+const getButtons = exportPayment => {
+  return {
+    record: [
+      {
+        value: "export",
+        onClick: exportPayment,
+      },
+    ],
+  }
+}
+
+const PaymentsHistory = ({
+  payments,
+  onRefresh,
+  isDeleteable,
+  exportPayment,
+}) => (
   <div>
-    {onRefresh && <div onClick={onRefresh}>_Referesh_</div>}
-    {payments.list.map(p => (
-      <div key={p.id}>
-        <time>{Time.localeString(p.timestamp)}</time>
-        <div>{p.message}</div>
-        <div>
-          <b>Escrow balance:</b>
-          {p.escrowBalance}
-        </div>
-        <div>
-          <b>Balance:</b>
-          {p.balance}
-        </div>
-        <br />
-      </div>
-    ))}
-    {isDeleteable && <div>Delete</div>}
-    {payments.list.length ? "" : "Nothing to show"}
-    {payments.loading ? "Loading..." : ""}
+    {!payments.loading &&
+      !payments.error && (
+        <Table
+          title="Payments History"
+          fields={paymentsConfig.fields}
+          records={payments.list}
+          buttons={getButtons(exportPayment)}
+          // TODO: onScrollBottom={onScrollBottom}
+          onZeroRecords={<div>Nothing to show</div>}
+          isSelectable={false}
+        />
+      )}
+    {/* {isDeleteable && <div>Delete</div>} */}
+    {payments.loading ? <div>Loading...</div> : ""}
+    {payments.error && <div>{payments.error}</div>}
   </div>
 )
+
+const mapDispatchToProps = {
+  exportPayment: Actions.payments.PaymentsHistoryExportPayment,
+  onScrollBottom: Actions.payments.paymentsHistoryOnScrollBottom,
+}
 
 const mapStateToProps = state => ({
   payments: state.payments,
   isDeleteable: state.user.DeleteAllow || state.user.isAdmin,
 })
 
-export default connect(mapStateToProps)(PaymentsHistory)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PaymentsHistory)
