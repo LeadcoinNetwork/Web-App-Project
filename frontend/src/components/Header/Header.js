c4b3a6a960750ed4b59eb7304ab63e7b8c8a77b9
Meir Cohen <meir@leadcoin.network>
2018-06-18 17:51:48 +0300

    WIP - header links

--- frontend/src/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const Header = props => <header className="ldc-header" />
-
-export default Header
--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "Components/Header"
+import Header from "Containers/Header"
 import { connect } from "react-redux"
 import { notifications } from "../../actions/index"
 import { push } from "react-router-redux"
