959d9cc0ff3b9db5723884f2d7fc23bbda795413
Meir Cohen <meir@leadcoin.network>
2018-06-18 13:14:04 +0300

    snackbar config + stories

--- frontend/src/containers/Snackbar/Snackbar.js
+++ frontend/src/containers/Snackbar/Snackbar.js
@@ -2,7 +2,18 @@ import "react-toastify/dist/ReactToastify.css"
 
 import React from "react"
 import { ToastContainer } from "react-toastify"
+import { Flip } from "react-toastify"
 
-const Snackbar = () => <ToastContainer />
+const Snackbar = () => (
+  <ToastContainer
+    position="top-right"
+    autoClose={6000}
+    newestOnTop
+    closeOnClick
+    draggable
+    pauseOnHover
+    transition={Flip}
+  />
+)
 
 export default Snackbar
--- frontend/src/sagas/snackbar.js
+++ frontend/src/sagas/snackbar.js
@@ -5,6 +5,9 @@ import { toast } from "react-toastify"
 export default function* snackbar() {
   while (true) {
     let notification = yield take(types.NOTIFICATION_SHOW)
-    yield call(toast[notification.payload.type], notification.payload.message)
+    yield call(
+      toast[notification.payload.type] || toast,
+      notification.payload.message,
+    )
   }
 }
