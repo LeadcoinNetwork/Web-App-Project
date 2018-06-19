c3f027afb7f6ba513d0817824e9b6f3373c9ca32
Meir Cohen <meir@leadcoin.network>
2018-06-19 19:29:20 +0300

    add stories for side menu

--- frontend/src/containers/SideMenu/SideMenu.stories.js
+++ frontend/src/containers/SideMenu/SideMenu.stories.js
@@ -3,10 +3,25 @@ import { storiesOf } from "@storybook/react"
 import { createStoreAndStory } from "storybook-utils/withRouter"
 import { user } from "Actions"
 
-storiesOf("Containers/SideMenu", module).add("first", () => {
-  let { store, story } = createStoreAndStory({ path: "/sell-leads" })
+storiesOf("Containers/SideMenu", module)
+  .add("Sell", () => {
+    let { store, story } = createStoreAndStory({ path: "/sell-leads" })
 
-  store.dispatch(user.loggedIn({ id: 1 }))
+    store.dispatch(user.loggedIn({ id: 1 }))
 
-  return story
-})
+    return story
+  })
+  .add("Buy", () => {
+    let { store, story } = createStoreAndStory({ path: "/buy-leads" })
+
+    store.dispatch(user.loggedIn({ id: 1 }))
+
+    return story
+  })
+  .add("My", () => {
+    let { store, story } = createStoreAndStory({ path: "/my-leads" })
+
+    store.dispatch(user.loggedIn({ id: 1 }))
+
+    return story
+  })
