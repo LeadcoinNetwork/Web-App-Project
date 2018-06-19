4fee7f3517fffcb73f17efe04322d73dec17d2ab
Meir Cohen <meir@leadcoin.network>
2018-06-19 18:14:41 +0300

    Header now get pathname form App

--- frontend/src/containers/Header/Header.js
+++ frontend/src/containers/Header/Header.js
@@ -1,40 +1,25 @@
 import React from "react"
-import { connect } from "react-redux"
 import { Link } from "react-router-dom"
 import NotificationElement from "Containers/NotificationElement"
 
-class Header extends React.Component {
-  render() {
-    let path = this.props.location.pathname
+const Header = ({ path, loggedIn }) => (
+  <header className={`ldc-header${loggedIn ? " h-app-mode" : " h-sign-mode"}`}>
+    <Link to="/" className="logo-link" />
+    {path === "/login" ? (
+      <div className="sign-link">
+        <span>already have an account?</span>
+        <Link to="/signup">Start Now</Link>
+      </div>
+    ) : (
+      <div className="sign-link">
+        <span>don't have an account?</span>
+        <Link to="/login">Login Here</Link>
+      </div>
+    )}
+    <div className="ham-menu">
+      <NotificationElement />
+    </div>
+  </header>
+)
 
-    return (
-      <header
-        className={`ldc-header${
-          this.props.loggedIn ? " h-app-mode" : " h-sign-mode"
-        }`}
-      >
-        <Link to="/" className="logo-link" />
-        {path === "/login" ? (
-          <div className="sign-link">
-            <span>already have an account?</span>
-            <Link to="/signup">Start Now</Link>
-          </div>
-        ) : (
-          <div className="sign-link">
-            <span>don't have an account?</span>
-            <Link to="/login">Login Here</Link>
-          </div>
-        )}
-        <div className="ham-menu">
-          <NotificationElement />
-        </div>
-      </header>
-    )
-  }
-}
-
-const mapStateToProps = state => ({
-  location: state.routerReducer.location,
-})
-
-export default connect(mapStateToProps)(Header)
+export default Header
