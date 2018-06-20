dcac480282d42eb731e79d44a17caf5ce8bacc4e
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-06-20 18:35:25 +0300

    FRONTEND: Example saga, select laucnhed after reducer.

--- frontend/src/storybook-examples/storybook.saga.stories.js
+++ frontend/src/storybook-examples/storybook.saga.stories.js
@@ -2,7 +2,14 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import { storyReduxLogger } from "../storybook-utils/withRedux"
 import { createStore, applyMiddleware } from "redux"
-import { call, put, take, takeEvery, takeLatest } from "redux-saga/effects"
+import {
+  call,
+  put,
+  select,
+  take,
+  takeEvery,
+  takeLatest,
+} from "redux-saga/effects"
 import createSagaMiddleware from "redux-saga"
 
 storiesOf("Example Stories/saga").add("saga", () => {
@@ -11,7 +18,12 @@ storiesOf("Example Stories/saga").add("saga", () => {
 
   // Redux create Store
   var store = createStore(
-    () => {},
+    (type, action) => {
+      console.log("get action in reducer:", action)
+      if (action.type == "REDUX_EXAMPLE_ACTION_FROM_DISPATCH") {
+        return { i: action.i }
+      }
+    },
     [],
     applyMiddleware(sagaMiddleware, storyReduxLogger),
   )
@@ -20,7 +32,11 @@ storiesOf("Example Stories/saga").add("saga", () => {
 
   function* sagaListener() {
     // Wait and listen to this action
+    console.log("Saga Listener Before Take")
     yield take("REDUX_EXAMPLE_ACTION_FROM_DISPATCH")
+    console.log("BEFORE YIELD new State", JSON.stringify(x), x)
+    var x = yield select()
+    console.log("AFTER YIELD new State", JSON.stringify(x), x)
 
     // Here we can do side-effects
 
@@ -31,10 +47,11 @@ storiesOf("Example Stories/saga").add("saga", () => {
   return (
     <div
       onClick={() => {
-        store.dispatch({ type: "REDUX_EXAMPLE_ACTION_FROM_DISPATCH" })
+        alert("click dispatch")
+        store.dispatch({ type: "REDUX_EXAMPLE_ACTION_FROM_DISPATCH", i: 50 })
       }}
     >
-      Click to dispatch
+      Click to dispatch!
     </div>
   )
 })
