5b780c4f2ff3fcf3778e0dac317dd39c7d98b501
Erez Hochman <erez@leadcoin.network>
2018-06-20 15:38:18 +0300

    added error stories to CSVMapping

--- frontend/src/containers/CSVMapping/csvmapping.stories.js
+++ frontend/src/containers/CSVMapping/csvmapping.stories.js
@@ -31,7 +31,7 @@ storiesOf("Containers/CSVMapping")
     return story
   })
 
-  .add("CSVMapping - errors", () => {
+  .add("CSVMapping - price error", () => {
     var { store, story } = createStoreAndStory({
       component: CSVMapping,
     })
@@ -48,5 +48,46 @@ storiesOf("Containers/CSVMapping")
     ]))
     store.dispatch(actions.csvMapping.csvMappingAgreeToTerms(false))
     store.dispatch(actions.csvMapping.csvMappingFormChange('price',""))
+    store.dispatch(actions.csvMapping.csvMappingError(['price']))
+    return story
+  })
+  .add("CSVMapping - terms error", () => {
+    var { store, story } = createStoreAndStory({
+      component: CSVMapping,
+    })
+    store.dispatch(actions.csvMapping.csvMappingFormChange('batch_id','666'))
+    store.dispatch(actions.csvMapping.csvMappingGetFileFields([
+      "date", "name", "phone", "email", "state",
+      "city", "property type", "size", "budget",
+      "bedrooms", "floor", "specification"
+    ]))
+    store.dispatch(actions.csvMapping.csvMappingGetDbFields([
+      "date", "name", "phone", "email", "state",
+      "city", "property type", "size", "budget",
+      "bedrooms", "floor", "specification"
+    ]))
+    store.dispatch(actions.csvMapping.csvMappingAgreeToTerms(false))
+    store.dispatch(actions.csvMapping.csvMappingFormChange('price',""))
+    store.dispatch(actions.csvMapping.csvMappingError(['agree_to_terms']))
+    return story
+  })
+  .add("CSVMapping - multiple errors", () => {
+    var { store, story } = createStoreAndStory({
+      component: CSVMapping,
+    })
+    store.dispatch(actions.csvMapping.csvMappingFormChange('batch_id','666'))
+    store.dispatch(actions.csvMapping.csvMappingGetFileFields([
+      "date", "name", "phone", "email", "state",
+      "city", "property type", "size", "budget",
+      "bedrooms", "floor", "specification"
+    ]))
+    store.dispatch(actions.csvMapping.csvMappingGetDbFields([
+      "date", "name", "phone", "email", "state",
+      "city", "property type", "size", "budget",
+      "bedrooms", "floor", "specification"
+    ]))
+    store.dispatch(actions.csvMapping.csvMappingAgreeToTerms(false))
+    store.dispatch(actions.csvMapping.csvMappingFormChange('price',""))
+    store.dispatch(actions.csvMapping.csvMappingError(['agree_to_terms', 'price']))
     return story
   })
