039f12bea65621f4a99c07b20a7f23b2ad407544
Erez Hochman <erez@leadcoin.network>
2018-06-25 10:08:24 +0300

    mini-commit

--- frontend/src/containers/AddLead/addLead.stories.js
+++ frontend/src/containers/AddLead/addLead.stories.js
@@ -16,23 +16,21 @@ storiesOf("Containers/AddLead")
     var { store, story } = createStoreAndStory({
       component: AddLead,
     })
-    store.dispatch(actions.addLead.addLeadGetDbFields([
-      "date", "name", "phone", "email", "state",
-      "city", "property type", "size", "budget",
-      "bedrooms", "floor", "specification", "price"
-    ]))
+    store.dispatch(actions.addLead.addLeadGetDbFields({
+      private: ['name', 'phone'],
+      public: ['floor', 'size']
+    }))
     return story
   })
   .add("AddLead - with erros", () => {
     var { store, story } = createStoreAndStory({
       component: AddLead,
     })
-    store.dispatch(actions.addLead.addLeadGetDbFields([
-      "date", "name", "phone", "email", "state",
-      "city", "property type", "size", "budget",
-      "bedrooms", "floor", "specification", "price"
-    ]))
+    store.dispatch(actions.addLead.addLeadGetDbFields({
+      private: ['name', 'phone'],
+      public: ['floor', 'size']
+    }))
     store.dispatch(actions.addLead.addLeadAddError('size','too short'))
-    store.dispatch(actions.addLead.addLeadAddError('city','does not exist'))
+    store.dispatch(actions.addLead.addLeadAddError('phone','is not valid'))
     return story
   })
