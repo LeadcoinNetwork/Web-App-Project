1bcd226d160f50d473891c94d97ab0e8459226b6
Meir Cohen <meir@leadcoin.network>
2018-06-26 11:26:32 +0300

    Button - add stories

--- frontend/src/components/Button/Button.stories.js
+++ frontend/src/components/Button/Button.stories.js
@@ -3,20 +3,38 @@ import { storiesOf } from "@storybook/react"
 import Button from "./Button"
 
 storiesOf("Components/Button")
-  .add("with label", () => <Button label="Hello" />)
-  .add("with click", () => (
+  .add("With Text", () => <Button label="Hello" />)
+  .add("Secondary", () => <Button label="Hello" isSecondary={true} />)
+  .add("With Click", () => (
     <Button label="Click me" onClick={() => alert("Clicked!")} />
   ))
-  .add("with loading witout text", () => (
+  .add("With Loading (default text)", () => (
     <Button label="Click me" loading={true} Click={() => alert("Clicked!")} />
   ))
-  .add("with App Style", () => <Button label="Click me" appStyle={true} />)
-
-  .add("with loading with text text", () => (
+  .add("with loading with text", () => (
     <Button
       label="Click me"
       loading={true}
-      loadingText="my text"
+      loadingLabel="my text"
       Click={() => alert("Clicked!")}
+    />
+  ))
+  .add("Disabled", () => <Button label="Click me" disabled={true} />)
+  .add("Secondary Disabled", () => (
+    <Button label="Click me" isSecondary={true} disabled={true} />
+  ))
+  .add("With App Style", () => <Button label="Click me" appStyle={true} />)
+  .add("With App Style Disabled", () => (
+    <Button label="Click me" appStyle={true} disabled={true} />
+  ))
+  .add("With App Style Secondary", () => (
+    <Button label="Click me" isSecondary={true} appStyle={true} />
+  ))
+  .add("With App Style Secondary Disabled", () => (
+    <Button
+      label="Click me"
+      appStyle={true}
+      isSecondary={true}
+      disabled={true}
     />
   ))
