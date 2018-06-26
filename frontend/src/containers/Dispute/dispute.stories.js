6a15503935e7edbff0867e086b4f077de5f73ba4
Meir Cohen <meir@leadcoin.network>
2018-06-26 16:08:44 +0300

    fix dispute story

--- frontend/src/containers/Dispute/dispute.stories.js
+++ frontend/src/containers/Dispute/dispute.stories.js
@@ -1,25 +1,24 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
-import * as actions from "actions"
 import { createStoreAndStory } from "storybook-utils/withRouter"
-import Dispute from "./Dispute"
+
 storiesOf("Containers/Dispute")
   .add("Dispute - empty", () => {
     var { store, story } = createStoreAndStory({
-      component: Dispute,
+      path: "/dispute",
     })
     return story
   })
   .add("Dispute - loading", () => {
     var { store, story } = createStoreAndStory({
-      component: Dispute,
+      path: "/dispute",
     })
     return story
   })
 
   .add("Dispute - error", () => {
     var { store, story } = createStoreAndStory({
-      component: Dispute,
+      path: "/dispute",
     })
     return story
   })
