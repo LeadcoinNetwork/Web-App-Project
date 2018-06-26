e252cfd80a7099f2639c67c2e290b14be756b086
Meir Cohen <meir@leadcoin.network>
2018-06-26 16:23:25 +0300

    fix withdraw container and stories

--- frontend/src/containers/Withdraw/Withdraw.js
+++ frontend/src/containers/Withdraw/Withdraw.js
@@ -1,25 +1,7 @@
 import React from "react"
-import { storiesOf } from "@storybook/react"
-import * as actions from "actions"
-import { createStoreAndStory } from "storybook-utils/withRouter"
 
-storiesOf("Containers/Withdraw")
-  .add("empty", () => {
-    var { store, story } = createStoreAndStory({
-      path: "/withdraw",
-    })
-    return story
-  })
-  .add("loading", () => {
-    var { store, story } = createStoreAndStory({
-      path: "/withdraw",
-    })
-    return story
-  })
+const Withdraw = () => {
+  return <h1>Withdraw</h1>
+}
 
-  .add("error", () => {
-    var { store, story } = createStoreAndStory({
-      path: "/withdraw",
-    })
-    return story
-  })
+export default Withdraw
