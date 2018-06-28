ff0d81b8981a38ca3e4bccc7fea37109b777c906
Meir Cohen <meir@leadcoin.network>
2018-06-28 20:14:05 +0300

    WIP - email confirmation

--- frontend/src/containers/EmailConfirmation/EmailConfirmation.js
+++ frontend/src/containers/EmailConfirmation/EmailConfirmation.js
@@ -1,55 +1,37 @@
 import React from "react"
-import axios from "axios"
 import Button from "Components/Button"
 
-class EmailConfirmation extends React.Component {
-  state = {
-    response: null,
-  }
-
-  resendEmail = () => {
-    axios.defaults.withCredentials = true
-    axios.defaults.headers.common["Authorization"] =
-      "Bearer " + this.props.token
-    axios
-      .get(`${process.env.BACKEND}/auth/resend-email`)
-      .then(response => {
-        this.setState({ response: "Email Sent!" })
-      })
-      .catch(error => {
-        if (error.response) {
-          // error originated from server
-          if (error.response.data.error) {
-            let errors = error.response.data.error.split("; ")
-            this.setState({ errors: errors })
-          }
-        } else if (error.request) {
-          // request made, no response though
-        } else {
-          // error was thrown during request setup
-        }
-      })
-  }
-
-  render() {
-    return (
-      <div className="emailConfirm">
-        <div> We sent you an email.</div>
-        <div> Please click on the link </div>
-        <div className="resend_button">
-          <div>
-            &nbsp;
-            <Button
-              onClick={this.resendEmail}
-              label="Resend"
-              /> 
-            &nbsp;
-          </div>
-        </div>
-        <div className="response">{this.props.confirmation_sent}</div>
-      </div>
-    )
-  }
-}
+const EmailConfirmation = props => (
+  <section className="ldc-email-confirmation">
+    We sent you an email, Please click on the link
+    <br />
+    <br />
+    <Button label="Resend" />
+  </section>
+)
 
 export default EmailConfirmation
+
+// resendEmail = () => {
+//   axios.defaults.withCredentials = true
+//   axios.defaults.headers.common["Authorization"] =
+//   "Bearer " + this.props.token
+//   axios
+//   .get(`${process.env.BACKEND}/auth/resend-email`)
+//   .then(response => {
+//     this.setState({ response: "Email Sent!" })
+//   })
+//   .catch(error => {
+//     if (error.response) {
+//       // error originated from server
+//       if (error.response.data.error) {
+//         let errors = error.response.data.error.split("; ")
+//         this.setState({ errors: errors })
+//       }
+//     } else if (error.request) {
+//       // request made, no response though
+//     } else {
+//       // error was thrown during request setup
+//     }
+//   })
+// }
