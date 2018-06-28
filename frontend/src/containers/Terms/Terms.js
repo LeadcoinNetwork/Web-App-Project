e6c70708d6ee803923ddbecc2eb9ba54269d537c
Noam Silver <noam@armadil.corp.co.il>
2018-06-28 19:05:28 +0300

    updated terms page

--- frontend/src/containers/Terms/Terms.js
+++ frontend/src/containers/Terms/Terms.js
@@ -2,9 +2,43 @@ import React from "react"
 
 const Terms = () => (
   <div className="terms">
-    <h1>Terms and Conditions</h1>
-    <h3>Disclaimer</h3>
-    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+    <h1>Terms and Conditions of Use</h1>
+    <div>Last updated: June 2018</div>
+    <div>
+      <p>Please read these Terms of Use carefully before using the http://www.leadcoin.network website and the LeadCoin Mobile App mobile application operated by LeadCoin.</p>
+      <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+      <p className="emphasize">
+        By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.
+      </p>
+    </div>
+    <div>
+      <h3>Purchases</h3>
+      <p>If you wish to purchase any product or service made available through the Service ("Purchase"), you may be asked to supply certain information relevant to your Purchase including, without limitation, your credit card number, the expiration date of your credit card and your billing address.</p>
+      <p>You represent and warrant that: (i) you have the legal right to use any credit card(s) or other payment method(s) in connection with any Purchase; and that (ii) the information you supply to us is true, correct and complete.</p>
+      <p>The Services may employ the use of third party services for the purpose of facilitating payment and the completion of Purchases. By submitting your information, you grant us the right to provide the information to these third parties subject to our Privacy Policy.</p>
+    </div>
+    <div>
+      <h3>Termination</h3>
+      <p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+      <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+    </div>
+    <div>
+      <h3>Content</h3>
+      <p>Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the content that you post and responsible for making sure that this content abides by the guidelines set by LeadCoin for appropriate content.</p>
+    </div>
+    <div>
+      <h3>Links To Other Websites</h3>
+      <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by LeadCoin.</p>
+      <p>LeadCoin has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that LeadCoin shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such websites or services.</p>
+    </div>
+    <div>
+      <h3>Changes</h3>
+      <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 (change this) days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+    </div>
+    <div>
+      <h3>Contact Us</h3>
+      <p>If you have any questions about these Terms, please contact us at info@leadcoin.network.</p>
+    </div>
   </div>
 )
 
