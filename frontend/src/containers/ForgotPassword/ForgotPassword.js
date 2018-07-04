1bbb3d6a2abd406aca0cefaeea28d574c20e1845
Meir Cohen <meir@leadcoin.network>
2018-07-04 11:41:26 +0300

    forgot password elements

--- frontend/src/containers/ForgotPassword/ForgotPassword.js
+++ frontend/src/containers/ForgotPassword/ForgotPassword.js
@@ -1,8 +1,16 @@
 import React from "react"
+import TextField from "Components/TextField"
+import Button from "Components/Button"
 
 const ForgotPassword = props => (
   <section className="ldc-forgot-password">
     <h1>Forgot your password?</h1>
+    <p>
+      Enter the email address associated with your account, and we’ll email you
+      a link to reset your password.
+    </p>
+    <TextField placeholder="Email" name="email" vlaue={""} />
+    <Button label="Send Reset Link" />
   </section>
 )
 
