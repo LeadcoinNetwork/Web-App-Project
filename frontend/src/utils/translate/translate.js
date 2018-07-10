ceb6e4a3c2f49ba278bdbfa4c36be22a305181e4
Noam Silver <noam@leadcoin.network>
2018-07-10 18:37:08 +0300

    no hebrew random and remove visual verify

--- frontend/src/utils/translate/translate.js
+++ frontend/src/utils/translate/translate.js
@@ -20,11 +20,11 @@ export default function t(textToTransalte) {
   const current = storage.getItem("current")
   const value = _.get(translateDatabase, [textToTransalte, current])
   if (value) {
-    return "☑" + value
-  } else if (current !== "en") {
-    return "☒" + getRandom(current, textToTransalte)
+    return value
+  } else if (current !== "en" && current !== "he") {
+    return getRandom(current, textToTransalte)
   } else {
-    return "☒" + textToTransalte
+    return textToTransalte
   }
 }
 
@@ -39,10 +39,7 @@ function getRandom(language, original) {
   }
   let str = ""
   for (let i = 0; i < original.length; i++) {
-    str +=
-      randomSource[language][
-        parseInt(Math.random() * randomSource[language].length)
-      ]
+    str += randomSource[language][parseInt(Math.random() * randomSource[language].length)]
   }
   return str
 }
