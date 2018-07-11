b38bd2f668b07566a1c404619eac4b23914ebbb4
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-11 15:45:08 +0300

    FRONTEND: Refactor update the rtl loader

--- frontend/webpack.rtl.duplicate.js
+++ frontend/webpack.rtl.duplicate.js
@@ -1,6 +1,32 @@
-var fs = require("fs")
 var rtlcss = require("rtlcss")
-var sassloader = require("sass-loader")
+/** 
+ For example
+
+  It take a CSS syntax and returns SASS Syntax
+ 
+ Takes:
+ html {
+  body {
+    float:right
+  }
+
+ .ldc-ltr {
+    html {
+      body {
+        float:right
+     }
+  }
+ }
+
+ .ldc-rtl {
+    html {
+      body {
+        float:left
+     }
+  }
+ }
+   
+ */
 module.exports.default = function(str) {
   str = ".ldc-ltr {" + str + "} .ldc-rtl {" + rtlcss.process(str) + "}"
   return str
