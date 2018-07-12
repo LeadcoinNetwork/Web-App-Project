95705994af6e0081db1b1c3c964800107b540ac3
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 14:22:33 +0300

    CI: small fix

--- scripts/local-frontend-build.sh
+++ scripts/local-frontend-build.sh
@@ -1,4 +1,8 @@
 cd frontend
-sudo npm i > dev/null
-sudo npm run build > dev/null
-sudo npm run storybook:build > dev/null
+echo 'Start NPM I in frontend'
+sudo npm i > /dev/null
+echo 'Start NPM run build in frontend'
+sudo npm run build > /dev/null
+echo 'Start NPM run storyboook:build in frontend'
+sudo npm run storybook:build > /dev/null
+echo 'finishs'
