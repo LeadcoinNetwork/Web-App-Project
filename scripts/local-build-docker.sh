2c6ef67500726fc4f1bbb5a60e5207708295a21f
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 16:44:02 +0300

    CI: try fix by removing sudo from docker login

--- scripts/local-build-docker.sh
+++ scripts/local-build-docker.sh
@@ -1,5 +1,6 @@
 set -e
 echo "start local-build-docker"
-sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-sudo docker build -t leadcoin/leadcoin . 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "docker after login"
+docker build -t leadcoin/leadcoin . 
 echo "finish local-build-docker"
