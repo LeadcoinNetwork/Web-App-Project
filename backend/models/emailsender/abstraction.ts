7c3477634e44d7ab59fed37563589759b4706082
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 13:50:36 +0300

    fix ts

--- backend/models/emailsender/abstraction.ts
+++ backend/models/emailsender/abstraction.ts
@@ -2,10 +2,15 @@ export default interface EmailSender {
   send({ from, to, subject, html }): Promise<any>
 }
 
-
 export interface EmailSenderClass {
   // new (send({ from, to, subject, html }): Promise<any>) :EmailSender
-  new ({frontend,backend}:{frontend:string,backend:string}) :EmailSender
+  new ({
+    frontend,
+    backend,
+  }: {
+    frontend: string
+    backend: string
+  }): EmailSender
 }
 
 export interface EmailProperties {
@@ -14,20 +19,3 @@ export interface EmailProperties {
   subject: string
   html: string
 }
-
-
-class moshe {
-  constructor({frontend,backend}:{frontend:string,backend:string}){
-    super()
-  }
-  send({ from, to, subject, html }): Promise<any>{
-  }
-
-}
-
-function hello(d:EmailSenderClass){
-  var y=new d()
-  y.
-}
-
-hello(moshe)
