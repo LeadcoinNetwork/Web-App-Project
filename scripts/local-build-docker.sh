9c899f5c322cdd304732399ea91dbe2817be6a91
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 16:36:06 +0300

    CI: move back to after_sucess conditional script

--- scripts/local-build-docker.sh
+++ scripts/local-build-docker.sh
@@ -1,6 +1,5 @@
 set -e
 echo "start local-build-docker"
-echo ${#DOCKER_USERNAME}
 sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 sudo docker build -t leadcoin/leadcoin . 
 echo "finish local-build-docker"
