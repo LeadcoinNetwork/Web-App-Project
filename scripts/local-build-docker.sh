836c86e32bf1ae920e43bf42a1e6c37d36fc03ed
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 17:09:28 +0300

    CI: move docker login to docker push script

--- scripts/local-build-docker.sh
+++ scripts/local-build-docker.sh
@@ -1,6 +1,4 @@
 set -e
 echo "start local-build-docker"
-sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-echo "docker after login"
 sudo docker build -t leadcoin/leadcoin . 
 echo "finish local-build-docker"
--- scripts/local-push-docker.sh
+++ scripts/local-push-docker.sh
@@ -1,2 +1,15 @@
-set -e
+echo "start docker push"
+echo "Loggin..."
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "Pushing..."
 sudo docker push leadcoin/leadcoin
+echo "Finish push"
+
+set -e
+echo "second time with -e"
+echo "start docker push"
+echo "Loggin..."
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "Pushing..."
+sudo docker push leadcoin/leadcoin
+echo "Finish push"
