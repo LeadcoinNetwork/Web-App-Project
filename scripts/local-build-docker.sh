37f9c773447a4ecc3e08816a5530c25ef08bd3d5
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 17:06:30 +0300

    CI: bring back sudo to docker

--- scripts/local-build-docker.sh
+++ scripts/local-build-docker.sh
@@ -1,6 +1,6 @@
 set -e
 echo "start local-build-docker"
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 echo "docker after login"
-docker build -t leadcoin/leadcoin . 
+sudo docker build -t leadcoin/leadcoin . 
 echo "finish local-build-docker"
