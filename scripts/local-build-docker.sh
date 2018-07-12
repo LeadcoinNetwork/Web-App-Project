92aa3edcd25c89876a6078d795ea12ba7f736a3d
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-12 14:09:59 +0300

    update deploy scripts

--- scripts/local-build-docker.sh
+++ scripts/local-build-docker.sh
@@ -1,4 +1,2 @@
-cd frontend
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t leadcoin/leadcoin . 
-cd ..
--- scripts/local-frontend-build.sh
+++ scripts/local-frontend-build.sh
@@ -1,4 +1,4 @@
 cd frontend
 npm i > dev/null
-- npm run build > dev/null
-- npm run storybook:build > dev/null
+npm run build > dev/null
+npm run storybook:build > dev/null
