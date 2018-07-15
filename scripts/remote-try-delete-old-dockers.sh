3af22f6ec7762f2fdd2e71f284d3f495e99d362a
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-15 19:10:28 +0300

    small fix

--- scripts/remote-try-delete-old-dockers.sh
+++ scripts/remote-try-delete-old-dockers.sh
@@ -1,2 +1,2 @@
 echo try delete old docker
-(sudo docker rmi $(docker images --filter "dangling=true" -q --no-trunc)) || true
+(sudo docker rmi $(sudo docker images --filter "dangling=true" -q --no-trunc)) || true
