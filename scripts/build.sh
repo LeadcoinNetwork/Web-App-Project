fcc48000489236978ad24f95ad23e980c8d834bf
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-17 18:54:01 +0300

    CI: Add gitlog.txt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 echo "start building"
+echo 'create gitlog.txt file'
+git log -1 > gitlog.txt
 echo "calling to local-frontend-build"
 scripts/local-frontend-build.sh
 echo "calling to local-build-docker"
