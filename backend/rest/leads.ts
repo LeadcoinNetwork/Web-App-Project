12a0995344f901de240020dbbc09667848ed5136
Erez Hochman <erez@leadcoin.network>
2018-07-18 11:41:06 +0300

    amended again, last time

--- backend/rest/leads.ts
+++ backend/rest/leads.ts
@@ -223,7 +223,7 @@ export function start({
         if (mock == 1) {
           let mock_lead = `{"date":1531902112073,"floor":4,"rooms":1,"size":18,"budget":165362,"city":"Pejkupeni","specification":"Ur uvo gi obaro.","state":"OR","propertyType":"Cardboard Box","ownerId":5,"name":"Virginia Estrada","phone":"(927) 820-4759","email":"agisemen@hastu.tl","active":true,"price":"4090856"}`
           return res.json({
-            list: JSON.parse(mock_lead),
+            list: [JSON.parse(mock_lead)],
             total: 1,
           })
         }
