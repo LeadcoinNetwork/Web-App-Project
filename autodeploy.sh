d9234a97bb39832373f8e96a223cd2516392c122
Aminadav Glickshtein <aminadav@leadcoin.network>
2018-07-19 18:08:54 +0000

    change wait to 10 seconds

--- autodeploy.sh
+++ autodeploy.sh
@@ -3,7 +3,10 @@ echo $force
 while true	
 do
 	result=$(git pull)
+	echo git pull
 	echo $result
+	echo "It will autodeploy every time the result is not Already up-to-date"
+	echo "To force autodeploy launch ./autodeploy.sh force"
 	if [ "$force" = "y" ]; then
 		donow=y
 	fi
@@ -27,5 +30,6 @@ do
 	fi
 	unset force
 	unset donow
-	sleep 5
+	echo "Waiting 10 seconds..."
+	sleep 10
 done
