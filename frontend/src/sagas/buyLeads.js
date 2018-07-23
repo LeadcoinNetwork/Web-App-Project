ae8e60547e427cad8d7ccf778676bda10b9ea75b
Meir Cohen <meir@leadcoin.network>
2018-07-23 15:24:43 +0300

    remove notification error from buyLead

--- frontend/src/sagas/buyLeads.js
+++ frontend/src/sagas/buyLeads.js
@@ -24,7 +24,6 @@ export default function* buyLeads(api) {
 
     if (res.error) {
       yield put(actions.leads.fetchError("BUY_LEADS"))
-      yield put(actions.app.notificationShow(res.error, "error"))
     } else {
       yield put(actions.leads.fetchSuccess("BUY_LEADS", res))
     }
