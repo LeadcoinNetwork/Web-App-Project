c19095e60d29b7962314731f84748aca807bf9e9
Noam Silver <noam@leadcoin.network>
2018-08-05 13:18:03 +0300

    added responsive design for the home page

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "react-router-dom"
 const Home = () => (
-  <div>
-    <h1>Here will be the homepage</h1>
-    <Link to="/buy-leads">-> Buy Leads</Link>
-    <br />
-    <hr />
-    <br />
-    <Link to="/sell-leads">-> Sell Leads</Link>
+  <div className="home">
+    <div className="container">
+      <Link to="/buy-leads" className="option buy">
+        Buy Leads
+      </Link>
+      <Link to="/sell-leads" className="option sell">
+        Sell Leads
+      </Link>
+    </div>
   </div>
 )
 
