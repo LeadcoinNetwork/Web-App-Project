8ae243e54f3e2548755c89d6e8ccc9a1f24fb905
Noam Silver <noam@leadcoin.network>
2018-08-08 13:07:25 +0300

    home page design

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -1,13 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import Button from "../../components/Button"
+import t from "../../utils/translate/translate"
+
 const Home = () => (
   <div className="home">
+    <p className="title">{t("Do you want to...")}</p>
     <div className="container">
       <Link to="/buy-leads" className="option buy">
-        Buy Leads
+        <Button label={t("Buy Leads")} />
+        <p className="description">
+          {t("Purchase hot leads for your business now!")}
+        </p>
       </Link>
       <Link to="/sell-leads" className="option sell">
-        Sell Leads
+        <Button label={t("Sell Leads")} />
+        <p className="description">
+          {t("Earn money by selling your unused leads to other professionals.")}
+        </p>
       </Link>
     </div>
   </div>
