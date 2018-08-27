ea8fd3f082f526ab91179159f0c74694ef8be4e7
Erez Hochman <erez@leadcoin.network>
2018-08-27 16:56:15 +0300

    [P] minor changes

--- puppets/index.js
+++ puppets/index.js
@@ -1,14 +1,11 @@
 //@ts-check
 const dotenv = require("dotenv")
 dotenv.config()
-console.log(process.env)
 const headless = process.env.HEADLESS === "true" ? true : false
 const url = process.env.URL
 const puppeteer = require("puppeteer")
 const Chance = require("chance")
-
-const InlineManualWeb = require("./inline-manual/web")
-const InlineManualMobile = require("./inline-manual/mobile")
+const inlineManual = require("./inline-manual")
 
 const chance = new Chance()
 const user = {
@@ -20,13 +17,12 @@ const user = {
   console.log("Waking up Puppets...")
   const webbrowser = await puppeteer.launch({ headless })
   const mobilebrowser = await puppeteer.launch({ headless })
-  let step = 0
   const webpage = await webbrowser.newPage()
   const mobilepage = await mobilebrowser.newPage()
   await webpage.goto(url)
   await mobilepage.goto(url)
-  const webStatus = InlineManualWeb(webpage)
-  const mobileStatus = InlineManualMobile(mobilepage)
+  const webStatus = inlineManual.web(webpage)
+  const mobileStatus = inlineManual.mobile(mobilepage)
   console.log({
     web: await webStatus,
     mobile: await mobileStatus,
