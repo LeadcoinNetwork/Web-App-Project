05be310b41a9e16303d5720931c21fccb0814170
Erez Hochman <erez@leadcoin.network>
2018-08-27 12:13:20 +0300

    [P] puppet finishes inline-manual successfully

--- puppets/index.js
+++ puppets/index.js
@@ -9,7 +9,8 @@ const user = {
   email: chance.email(),
 }
 ;(async () => {
-  const browser = await puppeteer.launch({ headless: false })
+  console.log("Puppet starts...")
+  const browser = await puppeteer.launch({ headless: true })
   let step = 0
   const page = await browser.newPage()
   const snapAndClick = async (selector = ".inmplayer-popover-button-next") => {
@@ -17,15 +18,23 @@ const user = {
     await page.click(selector)
   }
 
-  const navigationPromise = page.waitForNavigation()
   await page.goto("http://127.0.0.1.xip.io:8080")
+  console.log("Puppet is signing-up...")
+
+  // first, we sign up a new user
   await page.click(".sign-link a")
   await page.type(".ldc-textfield.fname", user.fname)
   await page.type(".ldc-textfield.lname", user.lname)
   await page.type(".ldc-textfield.email", user.email)
   await page.click(".sm-form .ldc-button")
   await page.waitForNavigation({ waitUntil: "networkidle0" })
+
+  // wait for inline-manual
+  console.log("Puppet is starting inline-manual...")
   await page.waitForSelector(".inmplayer-firstStep")
+
+  // buy leads
+  console.log("Puppet is learning to buy leads...")
   await snapAndClick()
   await snapAndClick()
   await snapAndClick(".buy .ldc-button")
@@ -51,12 +60,24 @@ const user = {
   await snapAndClick()
   await snapAndClick()
   await snapAndClick(".logo .logo-link")
-  //button-container
-  //lt-results-head
-  //await page.click('.bl-filters select:nth-child(2)')
-  // <-- $('.bl-filters select:nth-child(2)')
-  //await snap(page)
-  //await snap(page)
 
-  // await browser.close()
+  // sell leads
+  console.log("Puppet is learning to sell leads...")
+  await snapAndClick(".sell .ldc-button")
+  await snapAndClick()
+  await snapAndClick(".upload-links .csv-upload")
+  await snapAndClick()
+  await page.waitFor(500)
+  await snapAndClick()
+  await page.waitForSelector(".notification-badge")
+  await page.waitFor(2500)
+  await page.evaluate(() =>
+    document.querySelector(".ldc-notification-element").click(),
+  )
+  await snapAndClick()
+  await snapAndClick()
+  await page.waitForSelector("#twitter-share")
+  await snapAndClick("#twitter-share")
+  await browser.close()
+  console.log("Puppet ended without errors")
 })()
