e66b6ee5eef93ed35ea0f226ad479ba75ddd2799
Erez Hochman <erez@leadcoin.network>
2018-09-06 10:43:55 +0300

    [C] removed cypress from front-end

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,8 +1,19 @@
 describe("login", () => {
   it("should fail", () => {
     cy.visit("http://g.leadcoin.network/erez")
-    cy.get(".ldc-textfield.email").type("tester@leadcoin.network")
+    cy.clearCookies()
+    /*
+    cy.clearLocalStorage()
+    cy.window().then((win) => {
+      win.sessionStorage.clear()
+    })
+  */
+    cy.get(".ldc-textfield.email").type("erez+test@leadcoin.network")
     cy.get(".ldc-textfield.password").type("test123")
+    cy.get("button.ldc-button").click()
+    cy.get(".ldc-button")
+      .contains("Buy Leads")
+      .click()
     expect(true).to.equal(true)
   })
 })
